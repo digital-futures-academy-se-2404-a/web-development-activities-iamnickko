2a6f9b28a57ba57b11d9ae9d249b2b36fed72319
Nick Ko <nicholasedwardko@gmail.com>
2024-05-26 18:11:43 +0100

    completed javascriptDOM

--- js-activities/JavaScriptDOM/starter/src/index.js
+++ js-activities/JavaScriptDOM/starter/src/index.js
@@ -1,2 +1,25 @@
-const colours = ['red', 'green', 'blue', 'orange', 'lime', 'olive', 'black', 'yellow'];
+const colours = [
+  "red",
+  "green",
+  "blue",
+  "orange",
+  "lime",
+  "olive",
+  "black",
+  "yellow",
+];
 console.log(colours);
+
+const buildP = (placeholder, num) => {
+  let i = 0;
+  do {
+    const p = document.createElement("p");
+    const textNode = document.createTextNode("This is my new text node!");
+    p.style.color = colours[parseInt(Math.random() * colours.length)];
+    p.appendChild(textNode);
+    placeholder.appendChild(p);
+    i++;
+  } while (i < num);
+};
+const placeholder = document.querySelector("#placeholder");
+buildP(placeholder, 20);
